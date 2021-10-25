ca2e45fc16629bf6a03c9d81a83b9c45e56b81e3
joneirikboska <joneirikboska@gmail.com>
2021-10-25 11:31:33 +0200

    Change z-index of newsletter text

--- components/NewsletterSection.tsx
+++ components/NewsletterSection.tsx
@@ -3,7 +3,7 @@ import Newsletter from './forms/newsletter'
 
 const NewsletterSection = () => {
   return (
-    <div className='relative h-96 flex flex-col justify-center items-center text-center border-b-2 border-black'>
+    <div className='relative h-96 flex items-center justify-center border-b-2 border-black'>
       <Image
         src='/images/Newsletter BG.svg'
         alt=''
@@ -11,15 +11,17 @@ const NewsletterSection = () => {
         className='-z-10'
         objectFit='cover'
       />
-      <p className='font-bold text-2xl mb-4'>Newsletter</p>
-      <h2 className='font-serif text-5xl mb-4'>
-        Receive all the news about Oroko!
-      </h2>
-      <p className='font-serif mb-4 max-w-sm'>
-        Be part of the OROKO Family and stay in touch for all updates and future
-        plans.
-      </p>
-      <Newsletter />
+      <div className='z-10 flex flex-col items-center text-center justify-center'>
+        <p className='font-bold text-2xl mb-4'>Newsletter</p>
+        <h2 className='font-serif text-5xl mb-4'>
+          Receive all the news about Oroko!
+        </h2>
+        <p className='font-serif mb-4 max-w-sm'>
+          Be part of the OROKO Family and stay in touch for all updates and
+          future plans.
+        </p>
+        <Newsletter />
+      </div>
     </div>
   )
 }
