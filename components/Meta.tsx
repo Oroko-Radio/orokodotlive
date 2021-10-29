a2060215b305c4e56e9d3e8220287d03768ccd26
joneirikboska <joneirikboska@gmail.com>
2021-10-29 09:11:46 +0200

    Create meta tags

---
+++ components/Meta.tsx
@@ -0,0 +1,29 @@
+const Meta = () => {
+  return (
+    <>
+      <title>Oroko Radio - Stay Tuned</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta
+        name="description"
+        content="Oroko is a not-for-profit independent internet radio station based in Accra, Ghana. We aim to connect, inspire and empower through conversation, collaboration and community."
+      />
+      <meta property="og:title" content="Oroko Radio" />
+      <meta property="og:site_name" content="Oroko Radio" />
+      <meta
+        property="og:description"
+        content="Oroko is a not-for-profit independent internet radio station based in Accra, Ghana. We aim to connect, inspire and empower through conversation, collaboration and community."
+      />
+      <meta property="og:url" content="https://oroko.live/" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content="https://oroko.live/oroko_logo_full.jpg"
+      />
+      <meta property="og:image:width" content="1000" />
+      <meta property="og:image:height" content="1000" />
+      <meta property="og:image:alt" content="Oroko Radio Logo" />
+    </>
+  );
+};
+
+export default Meta;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
 import Head from "next/head";
 import type { NextPage } from "next";
 import Splash from "../views/splash";
+import Meta from "../components/Meta";
 
 const Home: NextPage = () => {
   return (
     <main>
       <Head>
-        <title>Oroko Radio - Stay Tuned</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="description"
-          content="Oroko is a not-for-profit independent internet radio station based in Accra, Ghana. We aim to connect, inspire and empower through conversation, collaboration and community."
-        />
+        <Meta />
       </Head>
       <Splash />
     </main>
