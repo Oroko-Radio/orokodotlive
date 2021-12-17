7faf30e90909a908f6eadb21d2cccb4afc5394d0
je-boska <joneirikboska@gmail.com>
2021-12-17 13:09:03 +0100

    Remove Logo from Splash

--- views/splash.tsx
+++ views/splash.tsx
@@ -3,14 +3,12 @@ import Banner from "../components/Banner";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
-import Logo from "../components/Logo";
 import NewsletterSection from "../components/Newsletter";
 import StayTuned from "../components/StayTuned";
 
 const Splash = () => {
   return (
     <div className="relative">
-      <Logo />
       <Hero />
       <Banner color="black">
         <StayTuned />
