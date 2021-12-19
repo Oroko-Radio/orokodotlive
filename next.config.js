90992b4d6c2286fc348b5ed36999f1e848dbf2ae
je-boska <joneirikboska@gmail.com>
2021-12-19 11:28:13 +0100

    Add image domain to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["images.ctfassets.net", "images.radio.co"],
+    domains: [
+      "images.ctfassets.net",
+      "images.radio.co",
+      "is2-ssl.mzstatic.com",
+    ],
   },
   reactStrictMode: true,
 };
