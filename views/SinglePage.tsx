a81480fc898fa2056ef973cf5461626638d0a8e9
je-boska <joneirikboska@gmail.com>
2021-12-27 17:46:34 +0100

    Adjust SinglePage responsive styles

--- views/SinglePage.tsx
+++ views/SinglePage.tsx
@@ -37,9 +37,9 @@ export default function SinglePage({
         <section className="relative border-2 border-black mb-6">
           {mixcloudLink && (
             <div className="flex md:absolute right-0 top-0 h-full border-b-2 md:border-b-0 md:border-l-2 border-black bg-orokoBlue text-black">
-              <div className="mx-8 my-2 rounded-full self-center bg-white border-black border-2 h-16 w-16 flex justify-center items-center">
+              <div className="mx-8 my-2 rounded-full self-center bg-white border-black border-2 h-16 w-16 lg:h-32 lg:w-32 flex justify-center items-center">
                 <button
-                  className="h-7 w-7 sm:h-9 sm:w-9 focus:outline-none focus:ring-4"
+                  className="h-7 w-7 lg:h-14 lg:w-14 sm:h-9 sm:w-9 focus:outline-none focus:ring-4"
                   onClick={handlePlayShow}
                 >
                   <Play />
@@ -49,14 +49,14 @@ export default function SinglePage({
           )}
           <div className="container max-w-3xl mx-auto">
             {date && (
-              <p className="my-4 font-sans font-semibold tracking-wide text-xl">
+              <p className="my-4 lg:mt-6 lg:mb-8 font-sans font-semibold tracking-wide text-xl lg:text-2xl">
                 {dayjs(date).format("ddd DD MMMM YYYY @ HH") + "H"}
               </p>
             )}
-            <h1 className="text-6xl mb-0 font-heading md:max-w-xl lg:max-w-3xl">
+            <h1 className="text-5xl md:text-6xl lg:text-7xl mb-0 font-heading md:max-w-xl lg:max-w-2xl xl:max-w-3xl">
               {title}
             </h1>
-            <h2 className="font-serif text-3xl mb-6">
+            <h2 className="font-serif text-4xl lg:text-6xl mb-6 lg:mb-10">
               {" "}
               With{" "}
               {artists &&
@@ -71,7 +71,7 @@ export default function SinglePage({
                   </span>
                 ))}
             </h2>
-            <div className="flex gap-1 mb-6">
+            <div className="flex gap-1 mb-6 lg:mb-8">
               {genres &&
                 genres.map(({ name }, idx) => (
                   <Tag text={name} color="white" key={idx} />
