59325ec606d12ae5dd24e020f947bf33dfb31a51
je-boska <joneirikboska@gmail.com>
2021-12-31 14:37:03 +0100

    Delete unused Play and Pause files

--- icons/play.tsx
+++
@@ -1,9 +0,0 @@
-export default function play() {
-  return (
-    <>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="-6 -2 40 40">
-        <path d="M35 17.5L0 35V0z" fill="currentColor" />
-      </svg>
-    </>
-  );
-}
