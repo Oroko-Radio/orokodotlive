fa604f188873ea11a80bc296a89b68082bfe6dd4
je-boska <joneirikboska@gmail.com>
2021-12-31 14:36:32 +0100

    Put paragraph back in menu on mobile

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -8,8 +8,8 @@ type MenuProps = {
 
 const Menu = ({ setIsMenuOpen }: MenuProps) => {
   return (
-    <div className="absolute top-0 p-20 w-full bg-orokoYellow text-black border-b-2 border-black z-40 shadow-2xl">
-      <nav className="md:mb-10">
+    <div className="absolute top-0 p-20 w-full max-h-full bg-orokoYellow text-black border-b-2 border-black z-40 shadow-2xl">
+      <nav className="mb-10">
         <ul className="flex flex-col md:flex-row justify-center items-center md:space-x-2 space-y-4 md:space-y-0">
           {links.map(({ name, url }, idx) => (
             <Link key={idx} href={url} passHref>
@@ -20,7 +20,7 @@ const Menu = ({ setIsMenuOpen }: MenuProps) => {
           ))}
         </ul>
       </nav>
-      <p className="hidden md:block text-3xl text-center">
+      <p className="block text-xl md:text-3xl text-center">
         Oroko is a not-for-profit independent internet radio station based in
         Accra, Ghana. We aim to connect, inspire and empower through
         conversation, collaboration and community.
