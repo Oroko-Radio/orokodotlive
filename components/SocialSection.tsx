0b94bd216d95977661eeaf48ced50c10fdbb511e
je-boska <joneirikboska@gmail.com>
2022-01-02 17:40:16 +0100

    Move Instagram first in SocialSection

--- components/SocialSection.tsx
+++ components/SocialSection.tsx
@@ -3,10 +3,10 @@ import SocialIcon from "./SocialIcon";
 const SocialSection = () => {
   return (
     <div className="flex gap-2">
+      <SocialIcon social="Instagram" />
       <SocialIcon social="TikTok" />
       <SocialIcon social="Twitter" />
       <SocialIcon social="Facebook" />
-      <SocialIcon social="Instagram" />
       <SocialIcon social="Discord" />
     </div>
   );
