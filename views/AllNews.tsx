7ffd9235d73f57bbc9fb4dc1196a949a6e540984
je-boska <joneirikboska@gmail.com>
2022-01-21 13:24:38 +0100

    Add 4 column layout to AllNews view

--- views/AllNews.tsx
+++ views/AllNews.tsx
@@ -8,7 +8,7 @@ const AllNews = ({ articles, heading = "All News" }) => {
   return (
     <div id="all-news">
       <h1 className="font-serif text-6xl m-8">{heading}</h1>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 m-8">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 m-8">
         {articles.map(
           (
             { title, date, slug, articleType, city, subtitle, coverImage },
