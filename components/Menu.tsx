522dd8955fe902c9b484564ca9316a9040d1ee6d
je-boska <joneirikboska@gmail.com>
2022-05-26 17:41:48 +0200

    Close mobile menu on click link

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -12,7 +12,7 @@ const Menu = ({ isMenuOpen, setIsMenuOpen }: MenuProps) => {
   return (
     <div
       className={cn(
-        "absolute top-0 p-4 pt-20 xl:p-20 xl:pt-28 w-full bg-orokoYellow text-black border-b-2 border-black z-40 shadow-3xl transition-transform",
+        "absolute top-0 p-4 pt-20 xl:p-20 xl:pt-28 w-full min-h-screen sm:min-h-0 bg-orokoYellow text-black border-b-2 border-black z-40 shadow-3xl transition-transform",
         {
           "-translate-y-full": !isMenuOpen,
           "translate-y-0": isMenuOpen,
@@ -37,14 +37,17 @@ const Menu = ({ isMenuOpen, setIsMenuOpen }: MenuProps) => {
         <ul className="sm:hidden">
           {links.map(({ name, url }, idx) => (
             <Link key={idx} href={url} passHref>
-              <li className="text-black text-2xl font-serif underline mb-2">
+              <li
+                className="text-black text-2xl underline mb-2 cursor-pointer"
+                onClick={() => setIsMenuOpen(false)}
+              >
                 {name}
               </li>
             </Link>
           ))}
         </ul>
       </nav>
-      <p className="font-serif block text-xl md:text-3xl xl:text-4xl sm:text-center">
+      <p className="font-serif block text-xl md:text-3xl xl:text-4xl sm:text-center max-w-md sm:max-w-none">
         Oroko is a not-for-profit independent internet radio station based in
         Accra, Ghana. We aim to connect, inspire and empower through
         conversation, collaboration and community.
