c294dc41d3047ca7f37a7a805b2f796a0b84e98f
je-boska <joneirikboska@gmail.com>
2023-10-26 11:42:20 +0200

    Display preview of client side image

--- pages/thumbnail-generator.tsx
+++ pages/thumbnail-generator.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import domtoimage from "dom-to-image";
 import cx from "classnames";
 import Meta from "../components/Meta";
@@ -6,8 +6,13 @@ import Button from "../components/ui/Button";
 
 export default function ThumbnailGenerator() {
   const thumbnail = useRef<HTMLDivElement | null>(null);
+
   const [title, setTitle] = useState<string>("");
-  const [format, setFormat] = useState<"square" | "portrait">("square");
+  const [aspectRatio, setAspectRatio] = useState<"square" | "portrait">(
+    "square"
+  );
+  const [bgFile, setBgFile] = useState<File | null>(null);
+  const [bgPreview, setBgPreview] = useState<string>("");
 
   function download() {
     domtoimage
@@ -24,53 +29,89 @@ export default function ThumbnailGenerator() {
       });
   }
 
+  useEffect(() => {
+    if (!bgFile) {
+      setBgPreview(null);
+      return;
+    }
+
+    const imageUrl = URL.createObjectURL(bgFile);
+    setBgPreview(imageUrl);
+    return () => URL.revokeObjectURL(imageUrl);
+  }, [bgFile]);
+
   return (
     <>
       <Meta title="Thumbnail Generator" />
       <div className="grid md:grid-cols-[1fr,3fr]">
-        <div>
-          <form>
+        <div className="grid items-center">
+          <form className="p-4">
+            <label htmlFor="title" className="block">
+              Title
+            </label>
+
             <input
-              className="border-black border"
+              id="title"
+              className="border-black border mb-4 block"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
             />
-            <fieldset>
-              <label htmlFor="square">Square</label>
-              <input
-                type="radio"
-                id="square"
-                onChange={(e) => {
-                  if (e.target.value === "on") {
-                    setFormat("square");
-                  }
-                }}
-                checked={format === "square"}
-              />
-              <label htmlFor="portrait">Portrait</label>
-              <input
-                type="radio"
-                id="portrait"
-                onChange={(e) => {
-                  if (e.target.value === "on") {
-                    setFormat("portrait");
-                  }
-                }}
-                checked={format === "portrait"}
-              />
+
+            <label htmlFor="image" className="block">
+              Image
+            </label>
+            <input
+              className="mb-4"
+              type="file"
+              accept=".jpg, .png, .jpeg, .gif, .tif, .tiff|image/*"
+              id="image"
+              onChange={(e) => {
+                setBgFile(e.target.files[0]);
+              }}
+            />
+
+            <fieldset className="pb-4">
+              <legend>Aspect ratio</legend>
+              <div className="flex items-center">
+                <input
+                  type="radio"
+                  id="square"
+                  onChange={(e) => {
+                    if (e.target.value === "on") {
+                      setAspectRatio("square");
+                    }
+                  }}
+                  checked={aspectRatio === "square"}
+                />
+                <label htmlFor="square">Square</label>
+              </div>
+              <div className="flex items-center">
+                <input
+                  type="radio"
+                  id="portrait"
+                  onChange={(e) => {
+                    if (e.target.value === "on") {
+                      setAspectRatio("portrait");
+                    }
+                  }}
+                  checked={aspectRatio === "portrait"}
+                />
+                <label htmlFor="portrait">Portrait</label>
+              </div>
             </fieldset>
             <Button onClick={download}>Download Jpeg</Button>
           </form>
         </div>
-        <div className="grid justify-center bg-black p-8">
+        <div className="grid justify-center bg-black">
           <div
             ref={thumbnail}
             className={cx("bg-white scale-50", {
-              "h-[1080px] w-[1080px]": format === "square",
-              "h-[1920px] w-[1080px]": format === "portrait",
+              "h-[1080px] w-[1080px]": aspectRatio === "square",
+              "h-[1920px] w-[1080px]": aspectRatio === "portrait",
             })}
           >
             <h2 className="text-2xl">{title}</h2>
+            <img src={bgPreview} alt="" />
           </div>
         </div>
       </div>
