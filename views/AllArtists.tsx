6374a5599449ec235869f34e2a51aad453ddd183
je-boska <joneirikboska@gmail.com>
2024-01-26 17:32:22 +0100

    Refactor artists page to use query params for state

--- views/AllArtists.tsx
+++ views/AllArtists.tsx
@@ -1,7 +1,10 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import Card from "../components/Card";
 import Tag from "../components/Tag";
 import { AllArtistEntry, CityInterface } from "../types/shared";
+import { usePathname, useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface AllArtistsProps {
   allArtists: AllArtistEntry[];
@@ -9,21 +12,34 @@ interface AllArtistsProps {
 }
 
 const AllArtists = ({ allArtists, cities }: AllArtistsProps) => {
-  const [residentFilter, setResidentFilter] = useState<string>("all");
-  const [cityFilter, setCityFilter] = useState<string>("all");
+  const searchParams = useSearchParams()!;
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const city = searchParams.get("city") || "all";
+  const filter = searchParams.get("resident-filter") || "all";
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
 
   const filteredArtists = useMemo<AllArtistEntry[]>(() => {
     return allArtists.filter((artist) => {
-      if (cityFilter === "all" || cityFilter === artist.city.name) {
-        if (residentFilter === "all") return artist;
-        if (residentFilter === "former residents")
-          return artist.isFormerResident;
-        if (residentFilter === "residents")
+      if (city === "all" || city === artist.city.name) {
+        if (filter === "all") return artist;
+        if (filter === "former residents") return artist.isFormerResident;
+        if (filter === "residents")
           return artist.isResident && !artist.isFormerResident;
-        if (residentFilter === "guests") return !artist.isResident;
+        if (filter === "guests") return !artist.isResident;
       }
     });
-  }, [cityFilter, residentFilter, allArtists]);
+  }, [city, filter, allArtists]);
 
   return (
     <div className="bg-orokoYellow px-4 md:px-8">
@@ -33,8 +49,13 @@ const AllArtists = ({ allArtists, cities }: AllArtistsProps) => {
         </h1>
         <select
           className="appearance-none pr-16 self-center bg-transparent border-black border-2 text-lg md:text-2xl text-black"
-          onChange={(e) => setResidentFilter(e.target.value)}
-          value={residentFilter}
+          onChange={(e) => {
+            router.push(
+              pathname +
+                "?" +
+                createQueryString("resident-filter", e.target.value)
+            );
+          }}
         >
           <option value="all">RESIDENTS & GUESTS</option>
           <option value="residents">RESIDENTS</option>
@@ -44,8 +65,12 @@ const AllArtists = ({ allArtists, cities }: AllArtistsProps) => {
       </div>
       <select
         className="md:hidden appearance-none self-center bg-transparent border-black border-2 text-lg md:text-2xl text-black"
-        onChange={(e) => setCityFilter(e.target.value)}
-        value={cityFilter}
+        onChange={(e) => {
+          router.push(
+            pathname + "?" + createQueryString("city", e.target.value)
+          );
+        }}
+        // value={cityFilter}
       >
         <option value="all">ALL CITIES</option>
         {cities.map(({ name }, idx) => (
@@ -55,17 +80,19 @@ const AllArtists = ({ allArtists, cities }: AllArtistsProps) => {
         ))}
       </select>
       <div className="hidden md:flex flex-wrap gap-1">
-        <div className="cursor-pointer" onClick={() => setCityFilter("all")}>
-          <Tag text="All" color={cityFilter === "all" ? "yellow" : null} />
-        </div>
+        <Link href={pathname + "?" + createQueryString("city", "all")} passHref>
+          <Tag text="All" color={city === "all" ? "yellow" : null} />
+        </Link>
         {cities.map(({ name }, idx) => (
-          <div
+          <Link
+            href={pathname + "?" + createQueryString("city", name)}
+            passHref
             key={idx}
-            onClick={() => setCityFilter(name)}
+            // onClick={() => setCityFilter(name)}
             className="cursor-pointer"
           >
-            <Tag color={name === cityFilter ? "yellow" : null} text={name} />
-          </div>
+            <Tag color={name === city ? "yellow" : null} text={name} />
+          </Link>
         ))}
       </div>
 
