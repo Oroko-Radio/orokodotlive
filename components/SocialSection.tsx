db51ac958e475243006270a9be648fd79cba3554
je-boska <joneirikboska@gmail.com>
2024-01-31 12:43:58 +0100

    Make social icons appear above nav arrows

--- components/SocialSection.tsx
+++ components/SocialSection.tsx
@@ -2,7 +2,7 @@ import SocialIcon from "./SocialIcon";
 
 const SocialSection = ({ className }: { className?: string }) => {
   return (
-    <div className={className + " flex gap-2"}>
+    <div className={className + " flex gap-2 z-20"}>
       <SocialIcon social="Instagram" />
       <SocialIcon social="TikTok" />
       <SocialIcon social="Twitter" />
