56ef995b3182afdaf1ed36b03f2ad1820ebf2eaf
je-boska <joneirikboska@gmail.com>
2025-07-05 15:47:38 +0000

    Add key prop to images in image block

--- components/ImageBlock.tsx
+++ components/ImageBlock.tsx
@@ -6,23 +6,19 @@ export default function ImageBlock({ images }: { images: Asset[] }) {
   return (
     <section className="image-block grid grid-cols-2 md:grid-cols-3 pb-6">
       {images.map((image, idx) => (
-        <div>
-          <Image
-            className={cn(
-              "object-cover h-full w-full border-black border-t-2",
-              {
-                "md:border-b-2": idx >= 3,
-                "md:border-r-2": idx !== 2 && idx !== 5,
-                "border-b-2": idx >= 4,
-                "border-r-2": idx % 2 === 0,
-              }
-            )}
-            src={image.url}
-            alt={image.title}
-            width={image.width}
-            height={image.height}
-          />
-        </div>
+        <Image
+          key={image.sys.id}
+          className={cn("object-cover h-full w-full border-black border-t-2", {
+            "md:border-b-2": idx >= 3,
+            "md:border-r-2": idx !== 2 && idx !== 5,
+            "border-b-2": idx >= 4,
+            "border-r-2": idx % 2 === 0,
+          })}
+          src={image.url}
+          alt={image.title}
+          width={image.width}
+          height={image.height}
+        />
       ))}
     </section>
   );
