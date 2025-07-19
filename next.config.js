476c2139cac77b42cfc58c16220b20bc3441b98b
je-boska <joneirikboska@gmail.com>
2025-07-19 15:35:59 +0200

    Upgrade all packages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["images.ctfassets.net"],
+    remotePatterns: [new URL("https://images.ctfassets.net/**")],
   },
   reactStrictMode: true,
   experimental: {
