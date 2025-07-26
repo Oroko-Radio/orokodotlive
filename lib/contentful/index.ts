f7e89462e035ac68e2489d42e0abd8007c255117
je-boska <joneirikboska@gmail.com>
2025-07-26 19:10:23 +0200

    Implore env vars

--- lib/contentful/index.ts
+++ lib/contentful/index.ts
@@ -49,6 +49,6 @@ export async function graphql(
 }
 
 export const client = createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN!,
 });
