94a50cd4fa7c7b043fe3131dc2dff937df741bfa
je-boska <joneirikboska@gmail.com>
2025-08-10 21:26:50 +0200

    Create Pages schema

---
+++ collections/Pages.ts
@@ -0,0 +1,59 @@
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import {
+  SlateToLexicalFeature,
+  defaultSlateConverters,
+} from "@payloadcms/richtext-lexical/migrate";
+import { CollectionConfig } from "payload";
+import { SlateHRConverter } from "./hrConverter";
+
+export const Pages: CollectionConfig = {
+  slug: "pages",
+  admin: {
+    useAsTitle: "title",
+  },
+  fields: [
+    {
+      name: "title",
+      label: "Title",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "slug",
+      label: "Slug",
+      type: "text",
+      unique: true,
+      required: true,
+    },
+    {
+      name: "subtitle",
+      label: "Subtitle",
+      type: "text",
+    },
+    {
+      name: "coverImage",
+      label: "Cover Image",
+      type: "upload",
+      relationTo: "media",
+    },
+    {
+      name: "content",
+      label: "Content",
+      type: "richText",
+      // NECESSARY FOR MIGRATION ONLY:
+      editor: lexicalEditor({
+        features: ({ defaultFeatures }) => [
+          ...defaultFeatures,
+          SlateToLexicalFeature({
+            converters: [...defaultSlateConverters, SlateHRConverter],
+            // disableHooks: true,
+          }),
+        ],
+      }),
+    },
+    {
+      name: "contentfulId",
+      type: "text",
+    },
+  ],
+};
